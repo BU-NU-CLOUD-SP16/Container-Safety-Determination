29227c275d200b19d6f8bed65ac9c1060fcc5f45
Rahul Sharma <rahulsharmaait@gmail.com>
2016-02-18 03:21:51 -0500

    Updated layout of csdproject.py

--- csdcheck.py
+++ csdcheck.py
@@ -1,11 +1,32 @@
+#####################################################################
+# File: csdcheck.py
+# Author: Jeremy Mwenda <jmwenda@bu.edu>
+# Desc:
+#
+# Target platform: Linux
+#
+# Dependencies
+# -----------------
+# 1. docker
+# 2. sdhash
+#
+# Installing dependencies
+# ----------------------------------
+# bash$
+#
+#####################################################################
+
 import os
 import sys
+import errno
 import shutil
 import subprocess as sub
 
+TEMP_DIR = "/tmp/csdproject"
+
 # cmd is a list: cmd and options if any
 def exec_cmd(cmd):
-    p = sub.Popen(cmd,stdout=sub.PIPE,stderr=sub.PIPE)
+    p = sub.Popen(cmd, stdout=sub.PIPE, stderr=sub.PIPE)
     output, errors = p.communicate()
     print errors
     #print output
@@ -15,36 +36,66 @@ def exec_cmd(cmd):
 
 def untarlayers(imagedir):    
     for d in os.listdir(imagedir):
-        layerdir = os.path.join(imagedir,d)
+        layerdir = os.path.join(imagedir, d)
         if os.path.isdir(layerdir):          
-            layertar =  os.path.join(layerdir,'layer.tar')            
-            exec_cmd(['tar','-xvf',layertar,'-C',layerdir])
+            layertar = os.path.join(layerdir, 'layer.tar')
+            exec_cmd(['tar', '-xvf', layertar, '-C', layerdir])
             os.remove(layertar)
 
 
-# imagename should be in the form image:tag
-# Example test: python csdcheck.py python:2.7.8-slim
-imagename = sys.argv[1]
+def untar_image(imagetar, imagedir):
+    print('untar...')
+    exec_cmd(['tar', '-xvf', imagetar, '-C', imagedir])
+    os.remove(imagetar)
+    untarlayers(imagedir)
 
-print('docker pull...')
-exec_cmd(['docker','pull',imagename])
 
-tmpdir = os.path.join(os.getcwd(), 'tmp')
-os.makedirs(tmpdir)
+def pull_image(imagename):
+    print('docker pull...')
+    exec_cmd(['docker', 'pull', imagename])
 
-imagetar = os.path.join(tmpdir,'image.tar')
-imagedir = os.path.join(tmpdir,'image')
-os.makedirs(imagedir)
 
-print('docker save...')
-exec_cmd(['docker','save','-o',imagetar,imagename])
-print('untar...')
-exec_cmd(['tar','-xvf',imagetar,'-C',imagedir])
-os.remove(imagetar)
-untarlayers(imagedir)
+def save_image(imagetar, imagename):
+    print('docker save...')
+    exec_cmd(['docker', 'save', '-o', imagetar, imagename])
 
-print('sdhashing...')
-outputfile = os.path.join(os.getcwd(), 'sdhashed')
-exec_cmd(['sdhash','-r',imagedir,'-o',outputfile])
 
-#todo cleanup: remove tmp dir
+def calculate_sdhash(imagedir, outputfile):
+    print('sdhashing...')
+    outputfile = os.path.join(os.getcwd(), 'sdhashed')
+    exec_cmd(['sdhash', '-r', imagedir, '-o', outputfile])
+
+
+def make_dir(path):
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise  # raises error
+        else:
+            pass
+
+
+def hash_image(imagename):
+    imagetar = os.path.join(tmpdir, 'image.tar')
+    imagedir = os.path.join(tmpdir, 'image')
+    make_dir(TEMP_DIR)
+    make_dir(imagetar)
+    make_dir(imagedir)
+
+    pull_image(imagename)
+    save_image(imagetar, imagename)
+    untar_image(imagetar, imagedir)
+
+    calculate_sdhash(imagedir, outputfile)
+
+    #todo cleanup: remove tmp dir
+
+
+
+if "__name__" == "__main__":
+    # imagename should be in the form image:tag
+    # Example test: python csdcheck.py python:2.7.8-slim
+    imagename = sys.argv[1]
+
+    hash_image(imagename)
