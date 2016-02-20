7b3925542b3615931465f1a2368b0626f902e109
Renqing Gao <gladius@bu.edu>
2016-02-20 00:21:43 -0500

    - saveDir function is now using os.getcwd in os.walk, dstdir not working(not absolute dir) after chdir - use python api for getHashByFileName function - update some comments

--- scripts/esDB.py
+++ scripts/esDB.py
@@ -2,7 +2,7 @@
 File requires input:
     a dest dir in esCfg file, we will find out files with suffix sdbf and put every line in that file into elasticsearch
 To Retrieve:
-    search for filename(with or without directory) from a index(e.g. ubuntu14.04), expected to get every 'bin' from different directory
+    search for filename(with directory) from a index(e.g. ubuntu14.04)
 '''
 from elasticsearch import Elasticsearch
 import json
@@ -18,31 +18,28 @@ nodeName = EsCfg['nodeName']
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 
-
-#put what ever hash in dstdir into elasticsearch
 #indexName => Ubuntu14.04
-#doc_type
-
 def get_SDBF_files(list_of_files):
     '''
     Filters all the filenames that are not '.sdbf' type.
     Takes as input a list of filenames (strings) and returns an updates list with only the sdbf filenames
 
-    :param list_of_files: list
-    :return: updated_list: list
+    :param list_of_files:   list, list of files in a directory
+    :return: updated_list:  list, list of sdbf files filtered
     '''
     return [filename for filename in list_of_files if filename.split('.')[-1] == 'sdbf']
 
+#TODO check time efficiency
 def saveDir(dstdir , indexName):
     '''
     Saves the all the hashes from dstdir into the Elasticsearch index provided
-    :param dstdir: string
-    :param indexName: string
+    :param dstdir:      string, directory where sdbf files are saved in previous step
+    :param indexName:   string, index name in elasticsearch, e.g. ubuntu14.04(must be lower case)
     :return:
     '''
-
+    i = 0
     os.chdir(dstdir)    #commandline 'cd dstdir'
-    for root, subdirs, files in os.walk(dstdir):
+    for root, subdirs, files in os.walk(os.getcwd()):
         sdbf_files = get_SDBF_files(files) #get a list of all the files with a '.sdbf' suffix
         for filename in sdbf_files: #iterate over all the files in the 'dstdir' directory
             file_path = os.path.join(root, filename) #get filepath
@@ -64,17 +61,19 @@ def saveDir(dstdir , indexName):
                 else:
                     #since doc type is not that important
                     docType = 'unknown'
+                #save item
                 put_in_Elastic(indexName, docType , dirFileName, line) #get suffix from filename and set docType
 
 def put_in_Elastic(indexName , docType , dirFileName , hashLine):
     '''
     Saves a single hash into the elasticsearch index provided
 
-    :param indexName:
-    :param docType:
-    :param dirFileName:
-    :param hashLine:
-    :return:
+    :param indexName:   string, index in elasticsearch e.g. ubuntu14.04(must be lower case)
+    :param docType:     string, get from file suffix, if none, use unknown
+    :param dirFileName: string, filepath + filename
+    :param hashLine:    string, sdhash code for file
+    :return:            no return currently
+    #TODO check RESTAPI return code and modify return value according to res
     '''
     print 'saving item:' + dirFileName
     res = es.index(
@@ -92,21 +91,18 @@ def put_in_Elastic(indexName , docType , dirFileName , hashLine):
 
 #search a file in elasticsearch
 def getHashByFileName(indexName , fileName):
-    #TODO replace it with python api
-    res = requests.get(host + ':' + str(port) + '/' + indexName + '/' + fileType + '/' + fileName)
-    #didn't consider multi-match or partial match yet
-    if res.ok:
-        print "found match"
-        print res.json()
-        return res.json()
-    else:
-        print "return code is not 200"
-        return res
-    #return result directly
+    # Notice: fileName has to be full dir + filename format
+    try:
+        resDict = es.get(index = indexName , id = fileName)
+        #result in dict
+        return resDict
+    except:
+        "Can't find match"
+        return
     
 
 def deleteIndex(indexName):
-    print "U sure you wanna delete it?(Y / N)"
+    print "U sure you wanna delete index: " + indexName + "?(Y / N)"
     ans = raw_input()
     if ans == 'Y' or ans == 'y':
         print 'deleting: ' + host + ':' + port + '/' + indexName
@@ -116,6 +112,12 @@ def deleteIndex(indexName):
 if __name__ == '__main__':
     #try save whatever in dest dir into index: ubuntu14.04
     res = saveDir(dstdir , 'ubuntu14.04')
-    #res = getHashByFileName('ubuntu14.04' , '000')
-    #deleteIndex('ubuntu14.04')
+    '''
+    res = getHashByFileName(
+        'ubuntu14.04' , 
+        '/home/gladius/Documents/16Spring/CloudComputing/ContainerCodeClassification/scripts/testHash/021/000/021000030'
+    )
+    deleteIndex('ubuntu14.04')
+    '''
+    
 
