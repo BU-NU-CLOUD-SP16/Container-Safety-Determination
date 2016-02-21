69ec44b56e6c9b0b98df2c4fb2cc09d36120c4dc
Renqing Gao <gladius@bu.edu>
2016-02-22 05:56:12 +0800

    - corrected according to code review

--- scripts/esDB.py
+++ scripts/esDB.py
@@ -11,30 +11,18 @@ import requests
 from esCfg import EsCfg
 
 
-class EsDB():
-    def __init__(self):
-        self.dstdir = EsCfg['dstdir']
-        self.srcdir = EsCfg['srcdir']
-        self.host = EsCfg['host']
-        self.port = EsCfg['port']
-        self.nodeName = EsCfg['nodeName']
-        self.es = self.getEsConn(self.host , self.port)
+class EsDB:
+    def __init__(self , configDict):
+        self.dstdir = configDict['dstdir']
+        self.srcdir = configDict['srcdir']
+        self.host = configDict['host']
+        self.port = configDict['port']
+        self.nodeName = configDict['nodeName']
+        self.es = self.get_es_conn(self.host, self.port)
 
-    def getEsConn(self, host , port):
+    def get_es_conn(self, host, port):
         print 'connecting to ' + host + ':' + port
         return Elasticsearch([{'host': host, 'port': port}])
-
-
-    '''
-    Filters all the filenames that are not '.sdbf' type.
-    Takes as input a list of filenames (strings) and returns an updates list with only the sdbf filenames
-
-    :param list_of_files:   list, list of files in a directory
-    :return: updated_list:  list, list of sdbf files filtered
-    '''
-    def get_SDBF_files(self , list_of_files):
-        #indexName => Ubuntu14.04   
-        return [filename for filename in list_of_files if filename.split('.')[-1] == 'sdbf']
 
     '''
     Saves the all the hashes from dstdir into the Elasticsearch index provided
@@ -42,29 +30,22 @@ class EsDB():
     :param indexName:   string, index name in elasticsearch, e.g. ubuntu14.04(must be lower case)
     :return:
     '''
-    def saveDir(self , dstdir , indexName):
-        #TODO check time efficiency
-        #commandline 'cd dstdir'
-        os.chdir(dstdir)    
+    def index_dir(self, path, indexName):
+        os.chdir(path)    
         for root, subdirs, files in os.walk(os.getcwd()):
             os.chdir(root)
-            #get a list of all the files with a '.sdbf' suffix
-            sdbf_files = files # get_SDBF_files(files)
-            #iterate over all the files in the 'dstdir' directory
-            for filename in sdbf_files: 
-                #get filepath
-                file_path = os.path.join(root, filename) 
-                # set file destination
-                file_dest = file_path.replace(self.srcdir, dstdir, 1)  
-                sdhashFile = open(filename , "r")
-                #iterate over each line in the sdbf file
-                for line in sdhashFile: 
-                    #get file name from sdhash(with suffix if exist)
-                    hashName = line.split(':')[3]
-                    #set all docType to default
-                    docType = 'default'
-                    #save item
-                    self.put_in_Elastic(indexName, docType , hashName, line) 
+            #iterate over all the files in path
+            for filename in files: 
+                #iterate over each line in the file
+                with open(filename, "r") as sdhashFile:
+                    for line in sdhashFile: 
+                        #get file name from sdhash(with suffix if exist)
+                        hashName = line.split(':')[3]
+                        #set all docType to default
+                        docType = 'default'
+                        #save item
+                        self.put_in_Elastic(indexName, docType, hashName, line) 
+
 
     '''
     Saves a single hash into the elasticsearch index provided
@@ -75,30 +56,26 @@ class EsDB():
     :return:            no return currently
     #TODO check RESTAPI return code and modify return value according to res
     '''
-    def put_in_Elastic(self , indexName , docType , dirFileName , hashLine):
+    def put_in_Elastic(self, indexName, docType, dirFileName, hashLine):
         print 'saving item:' + dirFileName
         res = self.es.index(
-            #ubuntu14.04
             index = indexName,
-            #bin
             doc_type = docType,
-
-            #/usr/bin/ls
             id = dirFileName,
-            # sdhash string, arg need to be json-like or dice?
             body = {'sdhash':hashLine}
         )
         print res
 
-    #search a file in elasticsearch
-    def getHashByFileName(self , indexName , fileName):
+    '''
+    search a file in elasticsearch
+    '''
+    def get_hash_by_fileName(self, indexName, fileName):
         # Notice: fileName has to be full dir + filename format
         try:
-            resDict = self.es.get(index = indexName , id = fileName)
-            #result in dict
+            resDict = self.es.get(index = indexName, id = fileName)
             return resDict
         except:
-            "Can't find match"
+            print "Can't find match"
             return
 
     '''
@@ -108,19 +85,19 @@ class EsDB():
     :param fileName:    string, should be filepath + filename that matches the reference
     :return:            no return currently
     '''
-    def judgeFileByFileName(self , indexName , fileName):
+    def judge_file_by_filename(self, indexName, fileName):
         #currently indexName should be ubuntu14.04
-        fileDict = getHashByFileName(indexName , fileName)
+        fileDict = get_hash_by_fileName(indexName, fileName)
         refSdhash = fileDict['_source']['sdhash']
         os.system('rm -r ./tempCompare')
         os.system('mkdir ./tempCompare')
         os.system('touch ./tempCompare/tempref.sdbf')
         #write searching result to reffile
-        temprefFile = open('./tempCompare/tempref.sdbf' , 'r+b')
+        temprefFile = open('./tempCompare/tempref.sdbf', 'r+b')
         temprefFile.write(refSdhash)
         temprefFile.close()
         #calc current file into tempobj.sdbf
-        #!!!!filename, where can I read self file
+        #!!!!filename, where can I read this file
         os.system('sdhash ./' + fileName + ' -o ./tempCompare/tempobj')
         #compare tempref and tempobj > tempRes
         os.system('sdhash -c ./tempCompare/tempref.sdbf ./tempCompare/tempobj.sdbf -t 0 > ./tempCompare/tempRes')
@@ -130,13 +107,13 @@ class EsDB():
         score = resfile.split('|')[-1]
         #remove \n
         score = score[:-1]
-        if score == "100\n":
+        if score == "100":
             print fileName + ' match 100%'
             pass
         else:   
             judgeIndex = 'judgeResult:' + indexName
             #TODO if use put_in_Elastic, here the body will be {'sdhash': resline}.  Better change the key
-            put_in_Elastic(judgeIndex, 'judgeResult' , fileName, resline)
+            put_in_Elastic(judgeIndex, 'judgeResult', fileName, resline)
             pass
         #clean up
         os.system('rm -r ./tempCompare')
@@ -147,40 +124,40 @@ class EsDB():
     :param refIndexName:   string, index name in elasticsearch, e.g. ubuntu14.04(must be lower case)
     :return:
     '''
-    def judgeDir(self , dstdir , refIndexName):
+    def judge_dir(self, dstdir, refIndexName):
         #TODO check time efficiency
         #commandline 'cd dstdir'
         os.chdir(dstdir)    
         for root, subdirs, files in os.walk(os.getcwd()):
             os.chdir(root)
-            #get a list of all the files with a '.sdbf' suffix
-            sdbf_files = files # get_SDBF_files(files)
-            #iterate over all the files in the 'dstdir' directory
-            for filename in sdbf_files: 
+            for filename in files: 
                 #get filepath
                 file_path = os.path.join(root, filename) 
                 #iterate over each line in the sdbf file
                 judgeFileByFileName(indexName, file_path) 
 
-    def deleteIndex(self , indexName):
+    def delete_index(self, indexName):
         print "U sure you wanna delete index: " + indexName + "?(Y / N)"
         ans = raw_input()
         if ans == 'Y' or ans == 'y':
-            print 'deleting: ' + self.host + ':' + self.port + '/' + indexName
-            res = requests.delete('http://' + self.host + ':' + self.port + '/' + indexName)
-            print res
+            if self.es.indices.exists(index=indexName):
+                res = self.es.indices.delete(index=indexName)
+                print res
+            else:
+                print 'index does not exist or already removed'
+
 
 if __name__ == '__main__':
     #try save whatever in dest dir into index: ubuntu14.04
-    testEsObj = EsDB()
-    res = testEsObj.saveDir(testEsObj.dstdir , 'ubuntu14.04')
+    testEsObj = EsDB(EsCfg)
+    # res = testEsObj.index_dir(testEsObj.dstdir, 'ubuntu14.04')
     '''
-    res = testEsObj.getHashByFileName(
-        'ubuntu14.04' , 
+    res = testEsObj.get_hash_by_fileName(
+        'ubuntu14.04', 
         '/home/gladius/Documents/16Spring/CloudComputing/ContainerCodeClassification/scripts/testHash/021/000/021000030'
     )
     '''
-    # testEsObj.deleteIndex('ubuntu14.04')
+    testEsObj.delete_index('ubuntu14.04')
     
     
 
