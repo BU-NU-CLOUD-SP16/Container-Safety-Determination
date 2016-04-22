01e3ca28fcc938ad87ddafb74b079994f822b8f7
Rahul Sharma <rahulsharmaait@gmail.com>
2016-04-22 19:06:02 +0000

    Merged flaskAPI.py and endpoint.py

--- endpoint/endpoint.py
+++ endpoint/endpoint.py
@@ -13,19 +13,22 @@
 # Dependencies
 # -----------------
 # 1. flask
+# 2. flask-cors
 #
 # Installing dependencies
 # ----------------------------------
-# bash$ sudo pip install flask
+# bash$ sudo pip install flask && pip install -U flask-cors
 #
 #####################################################################
 
 from flask import Flask
 from flask import request
 from flask.ext.cors import CORS
+from elasticsearch import Elasticsearch
 
 import ConfigParser
 import requests
+import hashlib
 import json
 import sys
 import os
@@ -106,6 +109,7 @@ def test():
                         break
     return "Done", 200
 
+
 @app.route('/scan/<container_id>')
 def scan_container(container_id):
     result = ''
@@ -120,7 +124,80 @@ def scan_container(container_id):
     except Exception as e:
         result = json.dumps({'error':'exception: ' + str(e) })
     return result, 200
-    
+
+
+@app.route('/get_judge_res/<judge_image_dir>')
+def get_judge_res(judge_image_dir):
+    es = Elasticsearch(EsCfg)
+    judge_image_dir = 'judgeresult:' + judge_image_dir
+    search_size = 20
+    search_offset = 0
+    print request.args
+    try:
+        if 'offset' in request.args:
+            search_offset = int(request.args.get('offset'))
+        if 'size' in request.args:
+            search_size = int(request.args.get('size'))
+        res_index = es.search(
+            index = judge_image_dir,
+            size = search_size,
+            from_=search_offset
+        )
+    except:
+        del(es)
+        return 'Error: index do not exist\n'
+    res_lst = []
+    for item in res_index['hits']['hits']:
+        res_lst.append(item['_source']['file'])
+    res_dict = {
+        'total' : res_index['hits']['total'],
+        'file_list' : res_lst,
+        'from_' : search_offset,
+        'size' : len(res_index['hits']['hits'])
+    }
+    json_res = json.dumps(res_dict)
+    del(es)
+    return json_res
+
+
+@app.route('/correct_false_warning/<judge_image_dir>')
+def correct_false_warning(judge_image_dir):
+    es = Elasticsearch(EsCfg)
+    if 'file_name' in request.args:
+        md5_file_name = hashlib.md5(request.args['file_name']).hexdigest()
+        print md5_file_name + ' for ' + request.args['file_name']
+    else:
+        del(es)
+        return 'Error: no file name in request\n'
+    judge_image_dir = 'judgeresult:' + judge_image_dir
+    try:
+        res = es.delete(index = judge_image_dir, doc_type = 'judgeResult', id = md5_file_name)
+    except:
+        del(es)
+        return 'Error: file do not exist\n'
+    del(es)
+    return json.dumps(res['_shards'])
+
+
+#which machine should run docker image? remotely or locally
+#and if word should be a list of arg?
+@app.route('/docker_run/')
+def docker_run():
+    es = Elasticsearch(EsCfg)
+    #check es again
+
+    #check finished, run docker image
+    try:
+        image_name = request.args.get('image_name')
+    except:
+        return 'can not get image_name in request\n'
+    arg_lst = request.args.getlist('args')
+    cmd = ['docker run', image_name] + arg_lst
+    cmd = ' '.join(cmd)
+    os.system(cmd)
+    return 'done\n'
+
+
 def log():
     print request.headers
     print request.args
